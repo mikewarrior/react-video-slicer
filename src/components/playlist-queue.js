83570ae66b595ce29ba6d81e820f43424307d678
Miguel Rueda <miguerueda86@yahoo.es>
2018-12-20 13:53:14 -0500

    adding delete functionality to playlist

--- src/components/playlist-queue.js
+++ src/components/playlist-queue.js
@@ -17,27 +17,63 @@ class PlaylistQueue extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.videoIndex !== this.props.videoIndex) {
-      this.setState({videoIndex: this.props.videoIndex})
+      this.setState({ videoIndex: this.props.videoIndex })
     }
+  }
+
+  onDeleteVideo = (event) => {
+    event.preventDefault();
+    let state = this.state;
+    let id = event.target.name;
+    state.clips.splice(id, 1);
+    this.setState({ clips: state.clips });
   }
 
   renderPlaylistClips = () => {
     const clips = this.state.clips || [];
+
     return clips.map((clip, index) => {
-      return (
-        <span key={index}
-          id={index}
-          className={`collection-item ${clips[this.state.videoIndex] === clip ? 'active' : ''}`}
-          name={clip.startTime !== undefined ? `${clip.startTime},${clip.endTime}` : ''}
-          onClick={this.props.onClick}
-        >{clip.name}{clip.startTime !== undefined ? ` - ${clip.startTime} s  to ${clip.endTime} s` : ''}</span>
-      );
+      if (index === 0) {
+        return (
+          <span key={index}
+            id={index}
+            className={`collection-item ${clips[this.state.videoIndex] === clip ? 'active' : ''}`}
+            name={clip.startTime !== undefined ? `${clip.startTime},${clip.endTime}` : ''}
+            onClick={this.props.onClick}
+          >{clip.name}{clip.startTime !== undefined ? ` - ${clip.startTime} s  to ${clip.endTime} s` : ''}</span>
+        );
+      } else {
+        return (
+          <span key={index}
+            id={index}
+            className={`collection-item ${clips[this.state.videoIndex] === clip ? 'active' : ''}`}
+            name={clip.startTime !== undefined ? `${clip.startTime},${clip.endTime}` : ''}
+            onClick={this.props.onClick}
+          >{clip.name}{clip.startTime !== undefined ? ` - ${clip.startTime} s  to ${clip.endTime} s` : ''}
+            <button
+              title="Edit"
+              className="btn-edit"
+              name={index}
+              type="button"
+              onClick={this.onDeleteVideo}
+            />
+            <button
+              title="Delete"
+              className="btn-delete"
+              name={index}
+              type="button"
+              onClick={this.onDeleteVideo}
+            />
+          </span>
+        );
+      }
     });
   }
 
   render() {
     return (
       <div className="playlist-queue" >
+        <h3>All your Videos</h3>
         <div id="playlist" className="collection">
           {this.renderPlaylistClips()}
         </div>
